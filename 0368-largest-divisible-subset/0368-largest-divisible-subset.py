35fdf2e36ce8898896f2105d003977e809c6923f
Praddy <69765504+pradyumnac26@users.noreply.github.com>
2025-04-06 10:21:03 -0600

    Time: 195 ms (38.91%), Space: 17.9 MB (92.2%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,43 +1,28 @@
 class Solution:
     def largestDivisibleSubset(self, nums):
-        # Edge case: if nums is empty, return an empty list
         if not nums:
             return []
 
-        # Sort the array to ensure that we only check in one direction (nums[i] % nums[j] where i > j)
         nums.sort()
+        n = len(nums)
+        dp = [1] * n         # dp[i] = length of longest divisible subset ending at i
+        prev = [-1] * n      # prev[i] = index of previous element in subset
+        max_len = 1
+        max_idx = 0
 
-        # dp[i] will store the size of the largest divisible subset that ends with nums[i]
-        dp = [1] * len(nums)
-
-        # prev[i] will store the index of the previous element in the subset
-        prev = [-1] * len(nums)
-
-        # Fill the dp and prev arrays
-        for i in range(1, len(nums)):
+        for i in range(n):
             for j in range(i):
-                if nums[i] % nums[j] == 0:  # nums[i] can be added to the subset of nums[j]
-                    if dp[j] + 1 > dp[i]:
-                        dp[i] = dp[j] + 1
-                        prev[i] = j
+                if nums[i] % nums[j] == 0 and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+            if dp[i] > max_len:
+                max_len = dp[i]
+                max_idx = i
 
-        # Find the index of the largest subset using built-in functions
-        max_size = max(dp)  # Maximum size of the divisible subset
-        max_index = dp.index(max_size)  # Index where the largest subset ends
+        # Reconstruct subset
+        res = []
+        while max_idx != -1:
+            res.append(nums[max_idx])
+            max_idx = prev[max_idx]
 
-        # Reconstruct the largest divisible subset
-        result = []
-        while max_index != -1:
-            result.append(nums[max_index])
-            max_index = prev[max_index]
-
-        # Reverse the result to get the subset in the correct order
-        return result[::-1]
-
-# Example usage:
-sol = Solution()
-nums1 = [1, 2, 3]
-print(sol.largestDivisibleSubset(nums1))  # Output: [1, 2]
-
-nums2 = [1, 2, 4, 8]
-print(sol.largestDivisibleSubset(nums2))  # Output: [1, 2, 4, 8]
+        return res[::-1]  # Reverse to get correct order
