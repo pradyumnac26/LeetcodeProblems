e2f050b26f7c3b2bc60d1bbec71278b08b6d2864
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-04-26 12:21:28 -0600

    Time: 48 ms (13.73%), Space: 53.3 MB (63.23%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    var a = "Hello World"
+    
+    return function(...args) {
+        return a
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
