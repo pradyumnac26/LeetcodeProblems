5dfcf58c38f4906399739cf4e5190fd3245aa230
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-05-25 15:19:21 -0600

    Time: 0 ms (100%), Space: 17.7 MB (58.49%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def rob_linear(nums: List[int]) -> int:
+            n = len(nums)
+            if n == 0:
+                return 0
+            if n == 1:
+                return nums[0]
+
+            dp = [0] * n
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, n):
+                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+
+            return dp[n - 1]
+
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
