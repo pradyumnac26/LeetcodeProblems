63562f634bf70961abe45a283fc92e09b1320d79
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-05-30 14:39:29 -0600

    Time: 11 ms (62.17%), Space: 20.1 MB (89.76%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0: 
+            return None
+
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2): 
+                list1 = lists[i]
+                list2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged.append(self.mergeLists(list1, list2))
+            lists = merged
+
+        return lists[0]
+
+    def mergeLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        temp = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+            temp = temp.next
+        if list1:
+            temp.next = list1
+        if list2:
+            temp.next = list2
+        return dummy.next
+
+        
