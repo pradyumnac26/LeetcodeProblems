fadf6dcbf8f8dad09045023fa8e70f4275d754bb
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-10-02 18:26:05 -0600

    Time: 66 ms (72.14%), Space: 29.8 MB (54.35%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -1,29 +1,30 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        # we can do dfs or bfs here we are just traversiving starting from the boundaries
-        rows, cols = len(grid), len(grid[0])
 
-        def dfs(r,c) : 
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == 0 : 
+        def dfs(r, c) : 
+            if r < 0 or r >= rows or c < 0 or c >= cols : 
                 return 
-            grid[r][c] = 0 
-            directions = [(0,1), (1,0), (0,-1), (-1,0)]
-            for dr,dc in directions : 
-                dfs(r+dr, c+dc)
+            if grid[r][c] == 0 : 
+                return
+            grid[r][c] = 0
+            dfs(r+1, c)
+            dfs(r, c+1)
+            dfs(r-1, c) 
+            dfs(r, c-1)
 
-
-        for r in range(rows) : 
-            for c in range(cols): 
-                if (r== 0 or c==0 or r == rows-1 or c == cols-1) and grid[r][c] == 1 :
-                    dfs(r,c)
-
-        count = 0 
+        rows, cols = len(grid), len(grid[0])
+        for i in range(rows) : 
+            for j in range(cols) : 
+                if i == 0 or j  == 0 or i == rows-1 or j == cols - 1 :
+                    if grid[i][j] == 1 : 
+                        dfs(i, j)
+                    
+        cnt = 0
         for i in range(rows) : 
             for j in range(cols) : 
                 if grid[i][j] == 1 : 
-                    count = count + 1
-        return count 
-
+                    cnt +=1 
+        return cnt
 
 
 
