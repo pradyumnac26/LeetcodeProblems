93d2c2faa7eb53dc9ffa1fd4cae6b7d348220f1e
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-10-06 11:01:55 -0600

    Time: 113 ms (81.4%), Space: 20.6 MB (55.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,30 +1,25 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        # multisource bfs
-        ROWS, COLS = len(mat), len(mat[0])
-        vis = [[0]*COLS for _ in range(ROWS)]
-        dist = [[0]*COLS for _ in range(ROWS)]
+        rows, cols = len(mat), len(mat[0])
+        dist = [[0]*cols for _ in range(rows)]
         q = deque()
-        for i in range(len(mat)): 
-            for j in range(COLS): 
+        for i in range(rows) : 
+            for j in range(cols) : 
                 if mat[i][j] == 0 : 
-                    q.append((i, j))
-                    vis[i][j] = 1
+                    q.append((i, j, 0))
         directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-
         while q : 
-            r, c= q.popleft()
-            for dr, dc in directions: 
-                new_row, new_col = r + dr, c + dc 
-                if 0<=new_row<ROWS and 0<=new_col<COLS and vis[new_row][new_col]==0 and mat[new_row][new_col] == 1 :
-                    q.append((new_row, new_col)) 
-                    dist[new_row][new_col] = 1 + dist[r][c]
-                    vis[new_row][new_col] = 1
-        return dist
+            r, c, d = q.popleft()
+            for dr, dc in directions : 
+                new_r, new_c = r + dr, c + dc 
+                if 0 <= new_r < rows and 0<=new_c < cols and mat[new_r][new_c] == 1 : 
+                    dist[new_r][new_c] = d + 1
+                    mat[new_r][new_c] = 0
+                    q.append((new_r, new_c, d+1))
+        return dist  
 
 
 
 
-        
-        
+
         
