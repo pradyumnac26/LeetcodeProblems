a3853c551b7ba71583bc01154de8df3b23dbce58
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-10-11 12:26:26 -0600

    Time: 63 ms (6.99%), Space: 17.8 MB (61.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,29 +1,21 @@
-from typing import List
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(i, res) : 
+            if i  == len(candidates) : 
+                return 
+            if sum(res) > target : 
+                return 
+            print(res)
+            if sum(res) == target : 
+                x.append(res)
+                return 
+
+            backtrack(i, res + [candidates[i]])
+            backtrack(i+1, res)
+
+        x = [] 
+
+        backtrack(0, [])
+        return x
+
         
-        def find_combinations(i, arr, target, ds, ans):
-            # Base case: If we've considered all elements
-            if i == len(arr):
-                if target == 0:
-                    ans.append(list(ds))  # Add a copy of the current combination
-                return
-            
-            # If the current element can be included (<= target)
-            if arr[i] <= target:
-                # Include the current element
-                ds.append(arr[i])
-                # Recur with the same index (allow repeated elements) and reduced target
-                find_combinations(i, arr, target - arr[i], ds, ans)
-                # Backtrack: Remove the last added element
-                ds.pop()
-            
-            # Exclude the current element and move to the next index
-            find_combinations(i + 1, arr, target, ds, ans)
-
-        ans = []
-        ds = []
-        find_combinations(0, candidates, target, ds, ans)
-        return list(ans)
-
