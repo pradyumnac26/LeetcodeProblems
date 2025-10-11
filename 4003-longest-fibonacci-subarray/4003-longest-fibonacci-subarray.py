5f891a3d703d05c8029841e10099aa68d11bdd7a
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-10-11 10:48:01 -0600

    Time: 143 ms (50%), Space: 34.5 MB (0%) - LeetHub

---
+++ 4003-longest-fibonacci-subarray/4003-longest-fibonacci-subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        # subarray problems. 
+        # 
+
+
+        def is_fibonacci(i) : 
+            if nums[i-2] + nums[i-1] == nums[i] : 
+                return True 
+            else : 
+                return False
+
+        
+        cnt = 0 
+        longest = 0
+        for i in range(len(nums)) : 
+            if i >=2 and is_fibonacci(i) : 
+                cnt +=1 
+            else : 
+                cnt = 1 
+            longest = max(longest, cnt+1)
+        return longest
+
+
