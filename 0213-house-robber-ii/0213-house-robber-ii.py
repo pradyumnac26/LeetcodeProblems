402bb2267b34265d60925f1dcbebeaa4e3276a61
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-10-14 10:44:00 -0600

    Time: 0 ms (100%), Space: 17.9 MB (38.71%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,27 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def rob_linear(nums: List[int]) -> int:
-            n = len(nums)
-            if n == 0:
-                return 0
-            if n == 1:
-                return nums[0]
-
-            dp = [0] * n
-            dp[0] = nums[0]
-            dp[1] = max(nums[0], nums[1])
-
-            for i in range(2, n):
-                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
-
-            return dp[n - 1]
-
-        n = len(nums)
-        if n == 0:
-            return 0
-        if n == 1:
+        n = len(nums) 
+        if n == 0 : 
+            return 0 
+        if n == 1 : 
             return nums[0]
-        if n == 2:
+        if n == 2 : 
             return max(nums[0], nums[1])
 
-        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
+        def f(i, nums, dp): 
+            if i < 0 : 
+                return 0 
+            if i == 0 : 
+                return nums[0] 
+            if dp[i]!=-1 : 
+                return dp[i]
+
+            p = nums[i] + f(i-2, nums, dp)
+            np = f(i-1, nums, dp)
+            dp[i] = max(p, np)
+            return dp[i]
+        dp1 = [-1]*(n+1) 
+        dp2 = [-1]*(n+1)
+        return max(f(n-2, nums[1:], dp1), f(n-2, nums[:-1], dp2))
+
+        
+        
