07a9de837f89c280c5e04a92702e9960b36b0c05
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-10-16 10:40:27 -0600

    Time: 35 ms (36.83%), Space: 18.7 MB (59.02%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -1,28 +1,18 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        @cache
-        def f(i,j) : 
-            if i == 0 : 
-                return matrix[0][j] 
-            if j < 0 or j >= m : 
-                return float('inf')
-            if dp[i][j] != -1 : 
-                return dp[i][j] 
-            up = matrix[i][j] + f(i-1,j)
-            ld = matrix[i][j] + (f(i - 1, j - 1) if j - 1 >= 0 else float('inf'))
-            rd = matrix[i][j] + (f(i - 1, j + 1) if j + 1 < m else float('inf'))
-            dp[i][j] = min(up,ld,rd) 
-            return dp[i][j]
-
-            
 
 
-        n = len(matrix)
-        m = len(matrix[0])
-        mini = float('inf')
-        dp = [[-1]*m for i in range (n)]
+        n, m = len(matrix), len(matrix[0])
+        dp = [[0]*m for _ in range(n)]
         for j in range(m) : 
-           mini = min(mini, f(n-1,j))
-        return mini
-        
-        
+            dp[n-1][j] = matrix[n-1][j] 
+
+        for i in range(n-2, -1, -1) : 
+            for j in range(m) : 
+                down, leftdown, rightdown = float('inf'), float('inf'), float('inf')
+                down = matrix[i][j] + dp[i+1][j] 
+                leftdown = matrix[i][j] + dp[i + 1][j - 1] if j - 1 >= 0 else float('inf')
+                rightdown = matrix[i][j] + dp[i + 1][j + 1] if j + 1 < m else float('inf')
+
+                dp[i][j] = min(down, leftdown, rightdown) 
+        return min(dp[0])
