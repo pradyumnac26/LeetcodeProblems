040d7bd8b1a65569fd12b8b7292ef3700c760237
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-10-21 16:24:30 -0600

    Time: 0 ms (100%), Space: 19.6 MB (60.67%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,26 +1,16 @@
 class Solution:
-    def insert(
-        self, intervals: List[List[int]], newInterval: List[int]
-    ) -> List[List[int]]:
-        n = len(intervals)
-        i = 0
-        res = []
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])  # ensure sorted by start
 
-        # Case 1: No overlapping before merging intervals
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
+        res = [intervals[0]]
 
-        # Case 2: Overlapping and merging intervals
-        while i < n and newInterval[1] >= intervals[i][0]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        res.append(newInterval)
-
-        # Case 3: No overlapping after merging newInterval
-        while i < n:
-            res.append(intervals[i])
-            i += 1
-
+        for start, end in intervals[1:]:
+            # compare with the last interval in result
+            if res[-1][1] >= start:  # overlap
+                res[-1][1] = max(res[-1][1], end)
+            else:
+                res.append([start, end])
         return res
+
+
