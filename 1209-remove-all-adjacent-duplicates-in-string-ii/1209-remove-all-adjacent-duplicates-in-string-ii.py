62bb7b2d84dbbdf594612f1208a3a01901beb4c8
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-02-11 10:47:55 -0600

    Time: 43 ms (58.88%), Space: 23.8 MB (36.25%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,21 +1,28 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        # append d in stack, append e in stack and then when u see the 3rd e u realise oh okay have already inserted 2 so lets pop k-1 times, and then u append d and then b twice and then c 3 times u realise its 
-        # equal to k-1 in stack alreready so u pop k-1 times and now u enter b and u see k-1 are equal in stack and again u pop, and now u see d and then.  aa.
-        st = [] # stores pairs of ch and count 
-        for ch in s : 
-            if st and st[-1][0] == ch : 
-                st[-1][1] += 1
+        # deeedbbcccbdaa -> ddbbcccbdaa
+        # ddbbbdaa -> dddaa -> aa -> which is the final answer... 
+        # so how to approach this.. 
+        # we need to keep track of the number, and if it is == k then we neeed to remove those.. 
+        # so lets dry run, so maybe we will put a seperate hashmap counter for ease ? lets try.. 
+        # no but we need contiguous , so howto do .? 
+        st = [] 
+        for i in s : 
+            if st and st[-1][0] == i : 
+                st[-1][1] +=1 
                 if st[-1][1] == k : 
-                    st.pop()
+                    st.pop() 
             
             else : 
-                st.append([ch, 1])
-        res = ""
-        for ch, count in st : 
-            res = res + count*ch
-
+                st.append([i, 1])
+        res = "" 
+        for i in st : 
+            ch, cnt = i 
+            res += ch*cnt 
         return res
+
+
+            
 
 
         
