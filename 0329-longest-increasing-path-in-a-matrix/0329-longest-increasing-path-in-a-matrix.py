04e53ba026e499475b29717b09936f7b7073ca82
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-02-15 18:57:29 -0600

    Time: 107 ms (89.18%), Space: 22.1 MB (52.39%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,26 +1,32 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        length = 0 
-        dp = [[-1]*COLS for _ in range(ROWS)]
+        directions = [(0, 1), (1, 0), (-1,0), (0,-1)] 
 
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        def dfs(r, c): 
-            if dp[r][c] !=-1: 
+
+        def dfs(r, c) : 
+            if dp[r][c] !=-1 : 
                 return dp[r][c]
 
             leng = 1
             for dr, dc in directions : 
-                new_row, new_col = r+dr, c+dc
-                if 0<=new_row<ROWS and 0<=new_col<COLS and matrix[r][c] < matrix[new_row][new_col]:
-                    leng = max(leng, 1 + dfs(new_row, new_col)) 
+                new_r = r + dr 
+                new_c = c + dc 
+                if new_r < ROWS and new_r >= 0 and new_c < COLS and new_c >= 0 : 
+                    if matrix[new_r][new_c] > matrix[r][c] : 
+                        leng = max( leng, 1+dfs(new_r, new_c))
             dp[r][c] = leng
             return dp[r][c]
 
-                
-              
-        for i in range(ROWS): 
-            for j in range(COLS): 
-                length = max(length, dfs(i, j) )
-        return length 
+
+
+        ROWS = len(matrix) 
+        COLS = len(matrix[0])
+        res = 0
+        dp = [[-1]*COLS for _ in range(ROWS)]
+
+        for i in range(len(matrix)): 
+            for j in range(len(matrix[0])): 
+                res = max(res, dfs(i, j))
+        return res
+
         
