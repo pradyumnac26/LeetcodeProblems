41748b25184eda67063de5e05efd719fe6f3a8c2
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-02-28 18:33:36 -0600

    Time: 4704 ms (23.56%), Space: 19.6 MB (53.75%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+
         
         def dfs(r, c, i) : 
             if i == len(word) : 
                 return True 
-            if r < 0 or c < 0 or r >= len(board) or c >= len(board[0]) or word[i]!=board[r][c] or (r, c) in path : 
+            
+            if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]) or word[i]!=board[r][c] or (r, c) in vis : 
                 return False 
-            path.add((r, c))
-            res = dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c-1, i+1) or dfs(r, c+1, i+1)
-            path.remove((r, c))
+
+            vis.add((r, c)) 
+            res = dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c+1, i+1) or dfs(r, c-1, i+1) 
+            vis.remove((r,c))
             return res
-            
-            
-        path = set()
-        for i in range(len(board)): 
-            for j in range(len(board[0])): 
-                if word[0] == board[i][j] and dfs(i,  j  ,0 ) : 
+
+
+
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        vis = set()
+        i = 0 
+        for r in range(len(board)) : 
+            for c in range(len(board[0])) : 
+                if word[0] == board[r][c] and dfs(r, c, 0) : 
                     return True 
         return False 
         
