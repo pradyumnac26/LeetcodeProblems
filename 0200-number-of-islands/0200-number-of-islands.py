93af225eb0a7caeb03aa81a839f059bed1095df0
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-02 16:43:28 -0600

    Time: 297 ms (7.68%), Space: 31.2 MB (10.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,38 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(r,c) : 
 
-            if r  < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) : 
-                return 
-            if grid[r][c] == "0" : 
-                return 
-            if vis[r][c] == True : 
+        def dfs(r, c) : 
+      
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) : 
                 return
 
-            vis[r][c] = True
+            if (r, c) in vis : 
+                return   
 
-            dfs(r, c+1)
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c-1)
+            if grid[r][c] == "0" : 
+                return 
+            
 
 
-        islands = 0 
-        rows, cols = len(grid), len(grid[0])
-        vis = [[False] * cols for _ in range(rows)]
+            vis.add((r, c))
+            for dr, dc in directions : 
+                new_dr, new_dc = r + dr, c + dc
+                if  0<=new_dr<len(grid) and 0<=new_dc<len(grid[0]) and grid[new_dr][new_dc] == "1" : 
+                    dfs(new_dr, new_dc)
 
-        for i in range(len(grid)) : 
-            for j in range(len(grid[0])): 
-                if grid[i][j] == "1" and vis[i][j]!=True: 
+            
+                
+        
+
+        cnt = 0 
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        vis = set()
+        for i in range(len(grid)): 
+            for j in range(len(grid[0])) : 
+                if grid[i][j] == "1" and (i, j) not in vis: 
                     dfs(i, j)
-                    islands+=1
-        return islands
+                    cnt+= 1
+        return cnt 
+
 
         
