6213995a6d4bea13c23d6443f5758bde81753212
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-02 11:57:46 -0600

    Time: 79 ms (77.7%), Space: 33.1 MB (68%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,30 +1,20 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        m, n = len(matrix), len(matrix[0])
-        visit = {}
-        
-        def dfs(r,c): 
-            if r >= m or c >= n : 
-                return 0 
-            if (r,c) in visit: 
-                return visit[(r, c)]
-            down = dfs(r+1, c)
-            right = dfs(r, c+1)
-            diag = dfs(r + 1, c+1)
-            if matrix[r][c] == '1' : 
-                visit[(r, c)] =  1 + min(down, right, diag)
-            else : 
-                visit[(r, c)] = 0 
-            return visit[(r, c)]
+        # largest square contianing onlu ones and return its area..
 
-        maxarea = 0
-        for i in range(m): 
-            for j in range(n): 
-                maxarea = max(maxarea, dfs(i, j))
-        return maxarea*maxarea
+        rows, cols = len(matrix), len(matrix[0]) 
+        dp = [[0]*(cols+1) for _ in range(rows+1)]
         
+        max_side= 0
+        for i in range(1, rows+1) : 
+            for j in range(1, cols+1) : 
+                if matrix[i-1][j-1] == "1" : 
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) 
+                    max_side = max(max_side, dp[i][j]) 
+        return max_side*max_side
 
 
-        
+
+
 
         
