60d02994a6d3e63826113f8cef6f4b1cbfff6d13
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-04 16:54:48 -0600

    Time: 0 ms (100%), Space: 19.2 MB (82.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @lru_cache(None)
-        def f(i) : 
-            if i <= 1 : 
-                return 1 
-            left = f(i-1)
-            right = f(i-2)
-            return left + right 
-
-        return f(n)
-
+        # it takes 2 steps to reach the top.. 
+        dp = [0]*(n+1)
+        dp[1] = 1 
+        if n >= 2 : 
+            dp[2] = 2
+        for i in range(3, n+1) : 
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
         
