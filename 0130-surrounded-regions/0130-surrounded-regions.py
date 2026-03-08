473ca9e4ba20612a5aa23381b40ecdaa242a874b
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-07 20:00:03 -0600

    Time: 5 ms (64.33%), Space: 22.4 MB (56.95%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,34 +3,37 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        ROWS, COLS = len(board), len(board[0])
-        visited = [[0]*COLS for _ in range(ROWS)]
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        def dfs(r, c) : 
+            board[r][c] = "T"
+            for dr, dc in directions : 
+                new_r, new_c = r + dr, c+ dc
 
-        def dfs(r, c): 
-            visited[r][c] = 1
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < ROWS and 0 <= nc < COLS and not visited[nr][nc] and board[nr][nc] == 'O':
-                    dfs(nr, nc)
+                if 0<=new_r<len(board) and 0<=new_c<len(board[0]) and board[new_r][new_c] == "O" : 
+                    dfs(new_r, new_c)
 
-        for i in range(ROWS): 
-            if board[i][0] == 'O' and not visited[i][0]:
+
+        for i in range(len(board)): 
+            if board[i][0] == "O" : 
                 dfs(i, 0)
-            if board[i][COLS-1] == 'O' and not visited[i][COLS-1]:
-                dfs(i, COLS-1)
-
-        for j in range(COLS): 
-            if board[0][j] == 'O' and not visited[0][j]:
+            if board[i][len(board[0])-1] == "O" : 
+                dfs(i, len(board[0])-1) 
+        for j in range(len(board[0])): 
+            if board[0][j] == "O" : 
                 dfs(0, j)
-            if board[ROWS-1][j] == 'O' and not visited[ROWS-1][j]:
-                dfs(ROWS-1, j)
+            if board[len(board)-1][j] == "O" :  
+                dfs(len(board)-1, j)
+        
+        for i in range(len(board)): 
+            for j in range(len(board[0])) : 
+                if board[i][j] == "O" : 
+                    board[i][j] = "X"
 
-        for i in range(ROWS):
-            for j in range(COLS): 
-                if board[i][j] == 'O' and not visited[i][j]:
-                    board[i][j] = 'X'
+                if board[i][j] == "T" : 
+                    board[i][j] = "O"
+        
 
+                
 
 
         
