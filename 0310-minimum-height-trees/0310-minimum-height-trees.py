a29178e4acbbb9aa83594707db94bf13b982dfbc
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-09 14:10:25 -0500

    Time: 77 ms (17.26%), Space: 28.5 MB (50.08%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,34 +1,38 @@
-from typing import List
-from collections import defaultdict, deque
-
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        # return the height of min heights root label  
+        # so first i will try with 0 as root, so go bfs and measure the height till the last node. 
+        # then try from 1 , and then try from 2, and then try from 3.. 
+        adj = defaultdict(list)
+        indegree = [0]*n
+        for u, v in edges : 
+            adj[u].append(v) 
+            adj[v].append(u)
+            indegree[u]+=1
+            indegree[v]+=1
+        print(adj)
         if n == 1:
             return [0]
 
-        adj = defaultdict(list)
-        deg = [0] * n
-
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-            deg[u] += 1
-            deg[v] += 1
-
-        # start with all leaves
-        q = deque([i for i in range(n) if deg[i] == 1])
-        remaining = n
-
-        # peel leaves layer by layer
-        while remaining > 2:
-            layer_size = len(q)
-            remaining -= layer_size
-
-            for _ in range(layer_size):
-                leaf = q.popleft()
-                for nei in adj[leaf]:
-                    deg[nei] -= 1
-                    if deg[nei] == 1:
+        q = deque()
+        for i in range(n): 
+            if indegree[i] == 1 : 
+                q.append(i)
+        remaining_nodes = n 
+        while remaining_nodes > 2 : 
+            leaf_count = len(q) 
+            remaining_nodes -= leaf_count
+            for _ in range(leaf_count): 
+                node = q.popleft()
+                for nei in adj[node]:
+                    indegree[nei]-=1 
+                    if indegree[nei] == 1 : 
                         q.append(nei)
-
         return list(q)
+        
+
+
+
+
+
+        
