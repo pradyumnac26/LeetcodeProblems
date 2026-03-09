61de6b291f501a9bfe58fe570b128998000cc146
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-08 19:51:25 -0500

    Time: 7 ms (46.18%), Space: 20.3 MB (92.5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # topological sort 
-        adj = defaultdict(list)
-        q = deque() 
-        res = [] 
-        indegree = [0]*numCourses
+        adj = defaultdict(list) 
+        q = deque()
+        indegree = [0]*numCourses  
         for u, v in prerequisites : 
-            adj[v].append(u) 
-            indegree[u]+=1 
-        for i in range(len(indegree)) : 
+            adj[v].append(u)
+            indegree[u]+=1
+
+        for i in range(len(indegree)): 
             if indegree[i] == 0 : 
                 q.append(i)
-        while q : 
+        res = []
+        while q :
             node = q.popleft() 
             res.append(node)
-            for nei in adj[node]: 
-                indegree[nei]-=1 
+            for nei in adj[node] : 
+                indegree[nei]-=1
                 if indegree[nei] == 0 : 
                     q.append(nei)
         if len(res) == numCourses : 
             return True 
         else : 
             return False
-
         
 
 
         
+
 
         
