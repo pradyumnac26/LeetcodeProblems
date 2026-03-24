c3aea2ac12de260c29cdbefa302465a95a39fb57
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-23 21:25:37 -0500

    Time: 0 ms (100%), Space: 19.2 MB (89.85%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        st = [] 
+        for i in range(len(s)): 
+            if s[i] != "]" : 
+                st.append(s[i])
+            else : 
+                substr = "" 
+                while st and st[-1] != "[" : 
+                    substr = st.pop() + substr
+                st.pop() 
+                k = "" 
+                while st and st[-1].isdigit() : 
+                    k =  st.pop() + k 
+                st.append(int(k)*substr) 
+        return "".join(st)
+
+        
