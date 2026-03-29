db47839c970c0a3e9e2e135edea05c06076286f1
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-03-29 09:10:58 -0500

    Time: 279 ms (51.81%), Space: 20 MB (96.79%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,28 +1,21 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # a transformation seq from word beginWord to endWord using wordList , 
+        if endWord not in wordList : 
+            return 0 
         s = set(wordList) 
-        q = deque([(beginWord, 1)]) 
-        if endWord not in s : 
-            return 0
+        q = deque([(beginWord, 1)])
         while q : 
             word, dist = q.popleft() 
             if word == endWord : 
-                return dist 
+                return dist
             for i in range(len(word)): 
-                for ch in "abcdefghijklmnopqrstuvwxyz" : 
-                    newWord = word[:i] + ch + word[i+1:] 
-                    if newWord in s : 
-                        q.append((newWord, dist+1)) 
-                        s.remove(newWord)
+                for j in "abcdefghijklmnopqrstuvwxyz" : 
+                    new_word = word[:i] + j + word[i+1:]  
+                    if new_word in s : 
+                        q.append((new_word, dist+1)) 
+                        s.remove(new_word)
         return 0
-
-
-
-
-
-
-            
-
-
-
+        
+        
         
