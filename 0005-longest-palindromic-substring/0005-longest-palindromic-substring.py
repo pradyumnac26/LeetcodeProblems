43de28d0ae87280f708f28f5f401d85e3df7e351
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2026-05-28 09:04:15 -0600

    Time: 303 ms (45.14%), Space: 19.3 MB (37.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,41 +1,36 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1:
+        res = s[0] 
+        maxi = 0
+        if len(s) == 1 : 
             return s
+         
+        for k in range(len(s)): 
+            i = k - 1 
+            j = k + 1 
+            while i >=0 and j < len(s) and s[i]==s[j] : 
+                resLen = j-i+1 
+                if resLen > maxi : 
+                    maxi = resLen 
+                    res = s[i: j+1] 
+                i = i -1 
+                j = j + 1 
+            
+            i = k 
+            j = k+1 
+            while i >=0 and j < len(s) and s[i]==s[j] : 
+                resLen = j-i+1 
+                if resLen > maxi : 
+                    maxi = resLen 
+                    res = s[i:j+1] 
+                i = i -1 
+                j = j +1 
+        return res
+            
+                
 
-        res = ""
-        longest = 0
+            
 
-        # odd length
-        for k in range(len(s)):
-            i = k - 1
-            j = k + 1
-            while i >= 0 and j < len(s):
-                if s[i] != s[j]:
-                    break
-                curr = j - i + 1
-                if curr > longest:
-                    longest = curr
-                    res = s[i:j+1]
-                i -= 1
-                j += 1
-
-        # even length
-        for k in range(len(s) - 1):
-            i = k
-            j = k + 1
-            while i >= 0 and j < len(s):
-                if s[i] != s[j]:
-                    break
-                curr = j - i + 1
-                if curr > longest:
-                    longest = curr
-                    res = s[i:j+1]
-                i -= 1
-                j += 1
-
-        # if no length>=2 palindrome found, return any single char
-        return res if res != "" else s[0]
 
 
         
